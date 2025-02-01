1ac522d33f3ee36ff38507b55e3bdcd8c453f846
joaompds <joaompsantos8@gmail.com>
2025-02-01 23:29:03 +0000

    feat: new playing card && food wiki

--- src/components/OnlineMembersSection/index.tsx
+++ src/components/OnlineMembersSection/index.tsx
@@ -13,6 +13,20 @@ export default function OnlineMembersSection() {
             href={`${siteConfig.baseUrl}docs/play`}
             className={"w-[93%] bg-cover bg-center relative rounded-xl h-[70vh] overflow-hidden " + Styles.bgOverlay}
          >
+            <div className="absolute flex flex-row text-white top-4 left-4 md:top-5 md:left-6 items-center gap-3">
+               <img src={siteConfig.favicon} className="rounded-lg h-14" />
+               <div className="flex flex-col w-full gap-0">
+                  <p className="text-lg font-bold mb-0">
+                     {siteConfig.title}
+                  </p>
+
+                  {data.status === "online" && (
+                     <p className="text-[var(--custom-muted)] mb-0">
+                        {data.online} players online
+                     </p>
+                  )}
+               </div>
+            </div>
             <div className="absolute inset-0 flex flex-col justify-end">
                <div className="flex flex-row justify-between p-6 px-9">
                      <div className="flex items-center text-white">
