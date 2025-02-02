afba2690c2aa19d8d415840f220a0c2b92d9604e
joaompds <joaompsantos8@gmail.com>
2025-02-02 19:15:13 +0000

    feat: make 'Crazy Biscuit' wiki

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,12 +1,14 @@
 import cn from "@site/src/lib/cn";
+import { createElement } from "react";
 
-export type Tags = Record<string, string>;
+export type Tags = Record<string, string | React.FC>;
 
 export type BannerProps = {
    src: string;
    title: string;
    tags?: Tags;
    className?: string;
+   imageClassName?: string;
 }
 
 export default function Banner({
@@ -14,6 +16,7 @@ export default function Banner({
    className,
    title,
    tags,
+   imageClassName,
 } : BannerProps) {
    return (
       <div className={cn("p-4 bg-[var(--custom-card)] flex flex-col gap-x-4 md:flex-row rounded-2xl shadow-lg", className)}>
@@ -24,14 +27,14 @@ export default function Banner({
                   <div className="flex flex-col gap-2">
                      {Object.entries(tags).map(([name, value]) => (
                         <span key={name} className="text-base">
-                           <span className="text-[var(--custom-text)] font-semibold">{name}</span>: <span className="text-[var(--custom-muted)]">{value}</span>
+                           <span className="text-[var(--custom-text)] font-semibold">{name}</span>: <span className="text-[var(--custom-muted)]">{typeof(value) === "string" ? <>{value}</> : createElement(value)}</span>
                         </span>
                      ))}
                   </div>
                </div>
             )}
          </div>
-         <img alt="banner" src={src} className="overflow-hidden rounded-2xl object-cover md:ml-auto w-[100%] mt-8 md:mt-0 md:w-[50%]" />
+         <img alt="banner" src={src} className={cn("overflow-hidden rounded-2xl object-cover md:ml-auto w-[100%] mt-8 md:mt-0 md:w-[50%]", imageClassName)} />
       </div>
    );
 }
